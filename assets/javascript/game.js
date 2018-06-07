9a26c823465a2e5a83a3865e0c9a9e4ccfa2c732
Christie Hennes <christiehennes@gmail.com>
2018-06-07 16:58:25 -0700

    Updated game logic to add crystal value to current score and to check if you won

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,8 +28,9 @@ let game = {
     initalizeGame: function() {
 
         this.generateTargetNumber();
-        this.generateCrystals();
-        this.displayWins();
+        this.generateCrystalValues();
+        this.displayCrystalImages();
+        this.updateScores();
 
 
     },
@@ -43,19 +44,7 @@ let game = {
 
     },
 
-    generateCrystals: function(){
-
-        //Clear out previos crystal values
-        this.crystalValues = [];
-
-        //Generate random values for crystals
-
-        for (let i=0; i < this.crystalImages.length; i++){
-            let value = Math.floor(Math.random() * 13);
-            this.crystalValues.push(value);
-            console.log("Value of cyrstal: " + this.crystalValues[i]);
-        }
-
+    displayCrystalImages: function(){
         //Set variable to crystals div
         let crystals = $("#crystals");
 
@@ -74,32 +63,117 @@ let game = {
             // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
             crystals.append(image);
         }
+    },
+
+    generateCrystalValues: function(){
+
+        //Clear out previous random crystal values
+        this.crystalValues = [];
+
+        //Generate random values for crystals
+        for (let i=0; i < this.crystalImages.length; i++){
+            let value = Math.floor(Math.random() * 13);
+            this.crystalValues.push(value);
+            console.log("Value of cyrstal: " + this.crystalValues[i]);
+        }
+
+        // //Set variable to crystals div
+        // let crystals = $("#crystals");
+
+        // //Generate img for each crystal
+        // for (let i = 0; i < this.crystalImages.length; i++){
+        //     let image = $("<img>");
+        //     image.addClass("crystal-image");
+
+        //     // Each imageCrystal will be given a src link to the crystal image
+        //     image.attr("src", this.crystalImages[i]);
+
+        //     // Each imageCrystal will be given a data attribute called data-crystalValue.
+        //     // This data attribute will be set equal to the array value.
+        //     image.attr("data-crystalvalue", this.crystalValues[i]);
+
+        //     // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+        //     crystals.append(image);
+        // }
 
     },
 
-    displayWins: function(){
+    updateScores: function(){
 
         $("#wins").html("Wins: " + this.wins);
         $("#losses").html("Losses: " + this.losses);
+        $("#current-score").html(this.currentCount);
 
+    },
+
+
+    addCrystalValue: function(value){
+
+        
+        //Convert value to a number
+        let numberValue = Number(value);
+
+        //Add it to your count 
+        this.currentCount = this.currentCount + numberValue;
+        console.log("Current Count after click: " + this.currentCount);
+
+        //Display your current score 
+        $("#current-score").html(this.currentCount);
+
+        //Check to see if you won/lost
+
+        if (this.currentCount === this.numberToGuess){
+            console.log("You win");
+            this.wonGame();
+        }
+        else if (this.currentCount > this.numberToGuess){
+            console.log("You lost");
+            this.lostGame();
+        }
+
+
+    },
+
+
+
+    wonGame: function(){
+
+        this.wins++;
+        this.currentCount = 0;
+        this.updateScores();
+        this.generateCrystalValues();
+        this.generateTargetNumber();
+
+    },
+
+    lostGame: function(){
+
+        this.losses++;
+        this.currentCount = 0;
+        this.updateScores();
+        this.generateCrystalValues();
+        this.generateTargetNumber();
 
     }
 
-
-
-
-    //Generate Number to Guess 
-
-    //Generate value for crystal 
-
-    //Add crystal value --> accepts the value of the button data attribute 
-
-    //Check if won game 
-
-    //
 }
 
 
 game.initalizeGame();
+
+
+
+$("#crystals").on("click", ".crystal-image", function(){
+
+    console.log("Clicked a stone");
+
+    //Grab the value of the clicked image 
+    let crystalValue = ($(this).attr("data-crystalvalue"));
+    console.log("Value of clicked crystal: " + crystalValue);
+
+    game.addCrystalValue(crystalValue);
+
+});
+
 
 
